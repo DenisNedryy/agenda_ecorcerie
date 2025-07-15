61dd360688450d12022799fdb372be38d6408824
Thibault <thibault.boutaud@gmx.fr>
2025-07-15 11:53:55 +0200

    premier commit

---
+++ front/public/js/classes/views/HomeView.js
@@ -0,0 +1,20 @@
+export class HomeView {
+
+    render() {
+        const el = document.getElementById("root");
+        if (el) {
+            el.innerHTML = `
+            <div class="home">
+                <div class="home__header bg_head"> 
+                    <h2>Accueil</h2>
+                </div>
+                <div class="home__content bg_main"> 
+                <div id="agenda-console"></div>
+                <div id="agenda"> 
+                    <img src="/public/assets/pictures/logos/logo_white.png"/>
+                </div>
+            </div>
+            `;
+        }
+    }
+} 
